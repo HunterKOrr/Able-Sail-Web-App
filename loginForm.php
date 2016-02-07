f79a8c85e1789bdbc33e1e02af2a483653c292a8
Jackson Moreland <12jdm5@queensu.ca>
2016-02-07 14:21:58 -0500

    Didn't do anything to index.html but tried to change the button lettering colouring to white.  Updated the updateInfo so there's some CSS.

--- loginForm.php
+++ loginForm.php
@@ -1,38 +1,43 @@
 <?php
  
-    session_start();
-    $user = $_POST['username'];
-    $password = $_POST['exPass'];
-    
-    $dbname = 'propheis_ablesail';
-    $server = 'kevinzuern.com';
-    $dbUser = 'propheis_able';
-    $dbPass = 'Ablesail';
+    if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
-
-    mysql_connect($server, $dbUser, $dbPass) or DIE('Connection to host isailed, perhaps the service is down!');
-    mysql_select_db($dbname) or DIE('Database name is not available!');
-
-    $user=mysql_real_escape_string($_POST['username']); 
-    $password=md5(mysql_real_escape_string($_POST['exPass'])); 
-    
-    //checks if user is in database
-    $query = "SELECT ID FROM user WHERE username='$user' and password='$password'";
-    $res = mysql_query($query);
-    $rows = mysql_num_rows($res);
-    if ($rows==1){
-        $_SESSION['username'] = $_POST['username'];
-        header("Location: updateInfo.html");
-    }
-
-    else{
-        echo "Username and Password not found";
-        header("Location: loginCreate.html");
-        // TODO - replace message with redirection to login page
-        //  header("Location: securedpage.php");
-    }
+        session_start();
+        $user = $_POST['username'];
+        $password = $_POST['exPass'];
         
+        $dbname = 'propheis_ablesail';
+        $server = 'kevinzuern.com';
+        $dbUser = 'propheis_able';
+        $dbPass = 'Ablesail';
+
+
+        mysql_connect($server, $dbUser, $dbPass) or DIE('Connection to host isailed, perhaps the service is down!');
+        mysql_select_db($dbname) or DIE('Database name is not available!');
+
+        $user=mysql_real_escape_string($_POST['username']); 
+        $password=md5(mysql_real_escape_string($_POST['exPass'])); 
+        
+        //checks if user is in database
+        $query = "SELECT ID FROM user WHERE username='$user' and password='$password'";
+        $res = mysql_query($query);
+        $rows = mysql_num_rows($res);
+        if ($rows==1){
+            $_SESSION['username'] = $_POST['username'];
+            header("Location: updateInfo.html");
+        }
+
+        else{
+            echo "Username and Password not found";
+            header("Location: loginCreate.html");
+            // TODO - replace message with redirection to login page
+            //  header("Location: securedpage.php");
+        }
+
+    }
+
     ?>
+    
 
 <!DOCTYPE html>
 <html lang="en">
@@ -48,23 +53,21 @@
 
 </head>
 <body>
-
-<form>
-  <div class="form-group">
-    <label for="username">Username address</label>
-    <input type="username" class="form-control" id="username" placeholder="Username">
-  </div>
-  <div class="form-group">
-    <label for="exPass">Password</label>
-    <input type="password" class="form-control" id="exPass" placeholder="Password">
-  </div>
-  <button type="submit" class="btn btn-default">Submit</button>
-</form>
-
-
-
     <section class="container">
         <h1>Sign In!</h1>
     </section>
+
+    <form action="loginForm.php" method='POST'>
+      <div class="form-group">
+        <label for="username">Username address</label>
+        <input type="username" class="form-control" id="username" placeholder="Username">
+      </div>
+      <div class="form-group">
+        <label for="exPass">Password</label>
+        <input type="password" class="form-control" id="exPass" placeholder="Password">
+      </div>
+      <button type="submit" class="btn btn-default">Submit</button>
+    </form>
+
 </body>
 </html>
